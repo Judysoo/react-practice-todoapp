58950c6918af5d276af63d5de925b79770357071
judysoo <judysoo1010@gmail.com>
2020-07-03 17:00:02 +0900

    0703Todo

---
+++ todo-app/src/components/TodoInsert.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {MdAdd} from 'reacr-icons/md';
+import './TodoInsert.scss';
+
+
+//새로운 항목을 입력하고 추가할 수 있는 컴포넌트. state를 통해 input의 상태 관리
+
+const TodoInsert = () => {
+    return(
+        <form className="TodoInsert">
+            <input placeholder="할 일을 입력하세요"
+        </form>
+    )
+}
