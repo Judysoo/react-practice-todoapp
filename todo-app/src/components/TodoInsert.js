e5f8c41872b37b33883a7019cf607e3a7f3f9326
judysoo <judysoo1010@gmail.com>
2020-07-07 16:28:47 +0900

    0707done

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 //새로운 항목을 입력하고 추가할 수 있는 컴포넌트. state를 통해 input의 상태 관리
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    //onsubmit은 Enter키 입력해도 이벤트 발생(onClick은 클릭이벤트만)
+    (e) => {
+      onInsert(value);
+      setValue(''); //value값 초기화
+      // submit이벤트는 브라우저에서 새로고침을 발생시킵니다.
+      // 이를 방지하기 위해 이 함수를 호출합니다.
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
         {/* MdAdd(+)라는 react-icons 불러오기  */}
@@ -18,5 +39,4 @@ const TodoInsert = () => {
 
 export default TodoInsert;
 
-/*새로운 항목을 입력하고 추가할 수 있는 컴포넌트
-state를 통해 인풋의 상태를 관리함*/
+
--- todo-app/src/components/TodoTemplate.js
+++ todo-app/src/components/TodoTemplate.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './TodoTemplate.scss';
 
+/*화면을 가운데에 정렬시켜 주며, 앱 타이틀(일정 관리) 를 보여주는 컴포넌트.
+children으로 내부 JSX를 props로 받아 와서 렌더링해 준다.*/
+
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
@@ -11,6 +14,3 @@ const TodoTemplate = ({ children }) => {
 };
 
 export default TodoTemplate;
-
-/*화면을 가운데에 정렬시켜 주며, 앱 타이틀(일정 관리) 를 보여주는 컴포넌트.
-children으로 내부 JSX를 props로 받아 와서 렌더링해 준다.*/
