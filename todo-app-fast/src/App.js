dcb80c24784df9ad061fd20e1015270db68f5c13
judysoo <judysoo1010@gmail.com>
2020-07-08 17:53:25 +0900

    0708

--- todo-app-fast/src/App.js
+++ todo-app-fast/src/App.js
@@ -1,50 +1,65 @@
-import React, { useState, useRef, useCallback } from 'react'; //상태 관리 useState
+import React, { useReducer, useRef, useCallback } from 'react'; //상태 관리 useState
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 1000; i++) {
+    array.push({
+      id: i,
+      text: '할 일' + i,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+//const todoReducer = (state,action) => {
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': //새로 추가
+      //{type:INSERT, todo:{id:1, text:'todo', checked:false}}
+      return todos.concat(action.todo);
+    case 'REMOVE': //제거
+      //{type:'REMOVE',id:1}
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': //토글
+      //{type:'REMOVE},id:1}
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
-    //useState 함수의 파라미터에 함수의 기본값을 넣어 주었다.
-    ///[상태값, 상태를 설정하는 함수]
-    { id: 1, text: '애들 발톱깎기', checked: false },
-    { id: 2, text: '리액트로 일정 어플리케이션 제작하기', checked: true },
-    { id: 3, text: '설빙 먹기', checked: false },
-  ]); //todos 배열 안에 들어있는 객체에 고유 id, 내용 text, 완료 여부 checked 값이 있다.
-  //이 배열은 TodoList.js에 props로 전달될 것이다.
-  const nextId = useRef(2501);
+  //useReducer(상태값  , 초기 상태, 초기 상태를 넣어주는 함수)
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  //컴포넌트가 맨 처음 렌더링될 떄만 createBulkTodos 함수 호출
+
+  const nextId = useRef(1001);
   //고윳값으로 사용될 ID
   //Ref를 사용하여 변수 담기
-  const onInsert = useCallback(
-    (text) => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1; //nextId 1씩 더하기
-    },
-    [todos],
-  );
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    }, //todos 배열에서 id로 항목 지우기  ↑filter 조건(T or F)
-    [todos],
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1; //nextId 1씩 더하기
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
+
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
@@ -55,3 +70,6 @@ const App = () => {
 };
 
 export default App;
+
+//미친 존나빨라짐 몇백만배로빨라짐;
+//useReducer를 사용합시다...
